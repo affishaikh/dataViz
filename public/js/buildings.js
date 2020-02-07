f11deaf480baa4842aacf34341bea6d5cbd746e0
affishaikh <aftabshk@In-Aftabshk.local>
2020-02-07 18:23:15 +0530

    Add x and y axis

--- public/js/buildings.js
+++ public/js/buildings.js
@@ -1,32 +1,58 @@
 const drawBuildings = buildings => {
-  const width = 400;
-  const height = 400;
+  const chartSize = { width: 600, height: 400 };
+  const margin = { left: 100, right: 10, top: 10, bottom: 150 };
+  const width = chartSize.width - margin.left - margin.right;
+  const height = chartSize.height - margin.top - margin.bottom;
   const y = d3
     .scaleLinear()
     .domain([0, 1000])
-    .range([0, 400]);
-
-  x = d3
+    .range([0, height]);
+  const x = d3
     .scaleBand()
     .range([0, width])
     .domain(_.map(buildings, "name"))
     .paddingInner(0.3)
     .paddingOuter(0.3);
-
+  const yAxis = d3
+    .axisLeft(y)
+    .tickFormat(d => `${d}m`)
+    .ticks(3);
+  const xAxis = d3.axisBottom(x);
   const toLine = b => `<strong>${b.name}</strong> <i>${b.height}</i>`;
-  document.querySelector("#chart-area").innerHTML = buildings
+  document.querySelector("#chart-data").innerHTML = buildings
     .map(toLine)
     .join("<hr/>");
 
   const svg = d3
-    .select("#chart-data")
+    .select("#chart-area")
     .append("svg")
-    .attr("width", width)
-    .attr("height", height);
+    .attr("width", chartSize.width)
+    .attr("height", chartSize.height);
 
-  const rects = svg.selectAll("rect").data(buildings);
+  const g = svg
+    .append("g")
+    .attr("transform", `translate(${margin.left},${margin.top})`);
 
-  newRects = rects
+  g.append("text")
+    .attr("class", "x axis-label")
+    .attr("x", width / 2)
+    .attr("y", height + 140)
+    .text("Tall Buildings");
+
+  g.append("text")
+    .attr("class", "y axis-label")
+    .attr("transform", "rotate(-90)")
+    .attr("x", -height / 2)
+    .attr("y", -60)
+    .text("Height (M)");
+
+  g.append("g")
+    .attr("class", "y-axis")
+    .call(yAxis);
+
+  const rects = g.selectAll("rect").data(buildings);
+
+  rects
     .enter()
     .append("rect")
     .attr("x", b => x(b.name))
@@ -35,7 +61,14 @@ const drawBuildings = buildings => {
     .attr("height", b => y(b.height))
     .attr("fill", "grey");
 
-  console.log(newRects);
+  g.append("g")
+    .attr("class", "x-axis")
+    .attr("transform", `translate(0, ${height})`)
+    .call(xAxis);
+  g.selectAll(".x-axis text")
+    .attr("x", -5)
+    .attr("y", 10)
+    .attr("transform", "rotate(-40)");
 };
 const main = () => {
   d3.json("data/buildings.json").then(drawBuildings);
