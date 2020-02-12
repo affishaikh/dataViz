799bbe938aa919c3a70066170e3c217f00b20d44
affishaikh <aftabshk@In-Aftabshk.local>
2020-02-12 23:57:54 +0530

    Updating chart

--- public/js/companies.js
+++ public/js/companies.js
@@ -1,18 +1,13 @@
-const drawCompanies = companies => {
+const drawCompanies = (companies, metaForViz) => {
   const chartSize = { width: 800, height: 600 };
   const margin = { left: 100, right: 10, top: 10, bottom: 150 };
   const width = chartSize.width - margin.left - margin.right;
   const height = chartSize.height - margin.top - margin.bottom;
   const colors = d3.scaleOrdinal(d3.schemeCategory10);
 
-  const toLine = c => `<strong>${c.Name}</strong> <i>${c.CMP}</i>`;
-  document.querySelector("#chart-data").innerHTML = companies
-    .map(toLine)
-    .join("<hr/>");
-
   const y = d3
     .scaleLinear()
-    .domain([0, _.maxBy(companies, c => +c.CMP).CMP])
+    .domain([0, _.maxBy(companies, metaForViz.key)[metaForViz.key]])
     .range([height, 0]);
 
   const x = d3
@@ -32,6 +27,7 @@ const drawCompanies = companies => {
   const svg = d3
     .select("#chart-area")
     .append("svg")
+    .attr("class", "chart-svg")
     .attr("width", chartSize.width)
     .attr("height", chartSize.height);
 
@@ -50,7 +46,7 @@ const drawCompanies = companies => {
     .attr("transform", "rotate(-90)")
     .attr("x", -height / 2)
     .attr("y", -60)
-    .text("CMP (Rs)");
+    .text(metaForViz.yAxisLabel);
 
   g.append("g")
     .attr("class", "y-axis")
@@ -62,10 +58,10 @@ const drawCompanies = companies => {
     .enter()
     .append("rect")
     .attr("x", c => x(c.Name))
-    .attr("y", c => y(c.CMP))
+    .attr("y", c => y(c[metaForViz.key]))
     .attr("width", x.bandwidth)
-    .attr("height", c => y(0) - y(c.CMP))
-    .attr("fill", c => colors(c.CMP));
+    .attr("height", c => y(0) - y(c[metaForViz.key]))
+    .attr("fill", c => colors(c[metaForViz.key]));
 
   g.append("g")
     .attr("class", "x-axis")
@@ -78,12 +74,54 @@ const drawCompanies = companies => {
     .attr("transform", "rotate(-40)");
 };
 
+const updatCompanies = (companies, meta) => {
+  const chartSize = { width: 800, height: 600 };
+  const margin = { left: 100, right: 10, top: 10, bottom: 150 };
+  const width = chartSize.width - margin.left - margin.right;
+  const height = chartSize.height - margin.top - margin.bottom;
+  const colors = d3.scaleOrdinal(d3.schemeCategory10);
+  const y = d3
+    .scaleLinear()
+    .domain([0, _.maxBy(companies, meta.key)[meta.key]])
+    .range([height, 0]);
+
+  const x = d3
+    .scaleBand()
+    .range([0, width])
+    .domain(_.map(companies, "Name"))
+    .paddingInner(0.3)
+    .paddingOuter(0.3);
+
+  d3.select(".y.axis-label").text(meta.yAxisLabel);
+
+  d3.selectAll("rect")
+    .data(companies)
+    .attr("x", c => x(c.Name))
+    .attr("y", c => y(c[meta.key]))
+    .attr("width", x.bandwidth)
+    .attr("height", c => y(0) - y(c[meta.key]))
+    .attr("fill", c => colors(c[meta.key]));
+};
+
+const drawChart = companies => {
+  let i = 1;
+  const metasForViz = [
+    { key: "CMP", yAxisLabel: "CMP (Rs)" },
+    { key: "PE", yAxisLabel: "PE (Rs)" },
+    { key: "MarketCap", yAxisLabel: "MarketCap (Rs)" }
+  ];
+  drawCompanies(companies, metasForViz[0]);
+  setInterval(() => {
+    updatCompanies(companies, metasForViz[i++ % 3]);
+  }, 2000);
+};
+
 const main = () => {
   d3.csv("data/companies.csv", c => {
     return {
       ...c,
       CMP: +c.CMP
     };
-  }).then(drawCompanies);
+  }).then(drawChart);
 };
 window.onload = main;
